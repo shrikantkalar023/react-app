b91557071ca4d5ccdfa56977f3c515059a22d345
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-30 23:54:53 +0530

    Learnt about Props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// {items: [], heading: str}
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-  // Hook
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
