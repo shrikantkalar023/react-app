149452ab24d1d359053025aacbf1f6f9b4ddfbbf
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-03 16:57:10 +0530

    using Immer to simplify Update logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./components/Button";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -8,11 +9,24 @@ function App() {
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        // draft is a proxy obj that records changes we going to apply to bug aray. copy of bug array.
+        const bug = draft.find((item) => item.id === 1);
+        bug ? (bug.fixed = true) : null;
+      })
+    );
   };
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title}
+          {bug.fixed === true ? "fixed" : "new"}
+        </p>
+      ))}
       <Button onClick={handleClick}>Update</Button>{" "}
     </div>
   );
