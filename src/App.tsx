95566ace1c13a96b5b64a2ecfe9d01b399c03211
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-03 18:13:08 +0530

    Sharing state betn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import { useState } from "react";
-import Button from "./components/Button";
-import produce from "immer";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
+  const [cartItems, setCartItems] = useState([
+    "product1",
+    "product2",
+    "product3",
   ]);
-
-  const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-    setBugs(
-      produce((draft) => {
-        // draft is a proxy obj that records changes we going to apply to bug aray. copy of bug array.
-        const bug = draft.find((item) => item.id === 1);
-        bug ? (bug.fixed = true) : null;
-      })
-    );
-  };
 
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title}
-          {bug.fixed === true ? "fixed" : "new"}
-        </p>
-      ))}
-      <Button onClick={handleClick}>Update</Button>{" "}
+      <NavBar cartItemsCount={cartItems.length}></NavBar>
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
     </div>
   );
 }
