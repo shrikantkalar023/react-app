a7b003abd389b0b2c5b9051329f871f3f08a63cc
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-04 18:35:50 +0530

    Accessing Input Fields and Ref Hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,30 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 const Form = () => {
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("clicked");
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
   };
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
