245be8ee33d3d1032e4e0bcc81f4a81fc099152e
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-04 19:09:02 +0530

    Controlled Components to read form input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,17 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    // if (nameRef.current !== null) person.name = nameRef.current.value;
+    // if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
     console.log(person);
   };
 
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  //   const nameRef = useRef<HTMLInputElement>(null);
+  //   const ageRef = useRef<HTMLInputElement>(null);
+  //   const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({ name: "", age: "" });
 
   return (
     <form onSubmit={handleSubmit}>
@@ -18,13 +19,29 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
