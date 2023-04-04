5e27c563126a73a79266ff37d308d7b2cf32a2e5
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-04 18:10:25 +0530

    Handling Form Submissions

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { FormEvent } from "react";
 
 const Form = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("clicked");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
