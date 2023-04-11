bbfa1396b4efdfe1cd1ad92ab70029aac9095ec2
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-12 05:10:16 +0530

    learnt about Effect cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 import ProductList from "./components/ProductList";
 
-function App() {
-  const [category, setCategory] = useState("");
+const connect = () => console.log("connecting");
+const disconnect = () => console.log("disconnecting");
 
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category}></ProductList>
-    </div>
-  );
+function App() {
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  });
+  return <div> </div>;
 }
 export default App;
