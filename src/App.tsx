ac00175cab8eb7dec6bd813939679807b0a0eb19
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-12 06:07:52 +0530

    Fetching data from server using axios lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { useEffect, useRef, useState } from "react";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("connecting");
-const disconnect = () => console.log("disconnecting");
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    connect();
-
-    return () => disconnect();
-  });
-  return <div> </div>;
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data));
+  }, []);
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}{" "}
+    </ul>
+  );
 }
 export default App;
