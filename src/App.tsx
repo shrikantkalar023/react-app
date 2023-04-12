5ac8c7436e3823189b278d96d95b3df8a7cd3db1
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-13 04:51:12 +0530

    Cancelling Fetch request & Show loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -8,31 +8,36 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
-
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  // get->promise->res/err
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
+    const controller = new AbortController();
+
+    setIsLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
         setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+    // .finally(() => setIsLoading(false));
+    // proper way is this
 
-    fetchUsers();
-
-    // .then((response) => setUsers(response.data))
-    // .catch((err) => setError(err.message));
+    return () => controller.abort();
   }, []);
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
