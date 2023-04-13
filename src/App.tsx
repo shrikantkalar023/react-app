c5361a6268ccb00e95a9eaeef2161c25c30dc4a3
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-04-13 06:19:28 +0530

    Updating Data at server

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,21 @@ function App() {
     setUsers(users.filter((u) => u.id !== user.id));
 
     axios
-      .delete("https://jsonplaceholder.typicode.com/xusers/" + user.id)
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Shrikant" };
+    setUsers([newUser, ...users]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
         setUsers(originalUsers);
@@ -50,6 +64,9 @@ function App() {
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+      <button onClick={addUser} className="btn btn-primary mb-3">
+        Add
+      </button>
       <ul className="list-group">
         {users.map((user) => (
           <li
